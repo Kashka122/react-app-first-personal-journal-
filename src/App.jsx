5564f9077aed0674fbab61b57cd7370d1e3df147
Oleg Kashentsev <okashentsev@gmail.com>
2024-12-17 15:58:59 +0500

    implement custom hook to recive data from localStorage and update it.

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,27 @@ import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import Body from './layouts/Body/Body';
 import JournalList from './components/JournalList/JournalList';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useEffect, useState } from 'react';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
 
+function mapData(data){
+	if(!data) {
+		return [];
+	}
+	return data.map(i=>({
+		...i, 
+		date:new Date(i.date)
+	}));
+}
 
 function App() {
-	const [currentData, setData] = useState([]);
-	
-	useEffect(()=> {
-		const data = JSON.parse(localStorage.getItem('data'));
-		if(data){
-			setData(data.map(pieceOfData =>{
-				return {
-					...pieceOfData,
-					date: new Date(pieceOfData.date) 
-				};
-			}));
-		}
-	}, []);	
-
-	useEffect(()=>{
-		if(currentData.length){
-			console.log('Запись');			
-			localStorage.setItem('data', JSON.stringify(currentData));
-		}		
-	},[currentData]);
+	const [currentData, setData] = useLocalStorage('data');	
 
 	const addData = (newData) =>{
-		setData(oldData => [...oldData, {
+		setData( [...mapData(currentData), {
 			text: newData.text,
 			title: newData.title,
 			date: new Date(newData.date),
-			id: oldData.length> 0 ? Math.max(...oldData.map(data => data.id)) + 1: 1
+			id: currentData.length> 0 ? Math.max(...currentData.map(data => data.id)) + 1: 1
 		}]);
 	};
 	
@@ -44,7 +34,7 @@ function App() {
 			<LeftPanel>
 				<Header />
 				<JournalAddButton></JournalAddButton>
-				<JournalList data={currentData}/>
+				<JournalList data={mapData(currentData)}/>
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addData}/>
